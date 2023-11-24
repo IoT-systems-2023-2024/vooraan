91ae3b31ca6f57f2bf61d9afb22c8c8bed5abf04
eliasd3 <elias.deruytter@gmail.com>
2023-11-24 15:55:17 +0100

    BLE1
    BLE mogelijk om integer naar gsm te versturen, dit om te testen

--- codeIOTvorken/src/main.cpp
+++ codeIOTvorken/src/main.cpp
@@ -1,18 +1,54 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <BLEPeripheral.h>
 
-// put function declarations here:
-int myFunction(int, int);
+BLEPeripheral ledPeripheral = BLEPeripheral();
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+BLEService ledService = BLEService("19b10000e8f2537e4f6cd104768a1214");
+BLECharCharacteristic ledCharacteristic = BLECharCharacteristic("19b10001e8f2537e4f6cd104768a1214", BLERead | BLEWrite);
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  ledPeripheral.setAdvertisedServiceUuid(ledService.uuid());
+  ledPeripheral.setAdvertisingInterval(3000);
+  ledPeripheral.addAttribute(ledService);
+  ledPeripheral.addAttribute(ledCharacteristic);
+  ledPeripheral.setLocalName("Nordic NRF52 DK");
+  ledPeripheral.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  BLECentral central = ledPeripheral.central();
+  int value = 0;
+  if (central)
+  {
+    while (central.connected())
+    {
+      ledCharacteristic.setValue(value);
+      ledPeripheral.setTxPower(4);
+      delay(1000);
+      if(value == 10){
+        value = 0;
+        central.disconnect();
+        ledPeripheral.setConnectable(false);
+      }
+      value++;
+      // if (ledCharacteristic.written())
+      // {
+      //   if (ledCharacteristic.value())
+      //   {
+      //     digitalWrite(LED_BUILTIN, HIGH);
+      //   }
+      //   else
+      //   {
+      //     digitalWrite(LED_BUILTIN, LOW);
+      //   }
+      // }
+    }
+    //q: i only want to send once a day to save a lot of power, how do i do this, i dont want it to be constantly advertising
+    //a: you can set the advertising interval to 0, and then call startAdvertising() when you want to advertise
+  }
 }
