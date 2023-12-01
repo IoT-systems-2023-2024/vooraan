e99001d391d5b27c71857bf9283bbe956e5d287d
eliasd3 <elias.deruytter@gmail.com>
2023-12-01 14:59:05 +0100

    aanmaak project

---
+++ codeVooraanMetZephyr/build/image_preload.cmake
@@ -0,0 +1,8 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52dk_nrf52832" CACHE INTERNAL "NCS child image controlled")
+set(BOARD_ROOT "c:/direct_test_mode;d:/school/master-iot/vooraan/bt-fund-main/lesson2/blefund_less2_exer1_solution;d:/school/master-iot/vooraan/codevooraanmetzephyr;D:/programmas/ncs/v2.5.0/nrf;D:/programmas/ncs/v2.5.0/zephyr" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "d:/School/Master-IOT/vooraan/codeVooraanMetZephyr/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "D:/programmas/ncs/toolchains/c57af46cb7/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
---
+++ codeVooraanMetZephyr/build/zephyr/zephyr.dts.d
@@ -0,0 +1,22 @@
+empty_file.o: D:/programmas/ncs/v2.5.0/zephyr/misc/empty_file.c \
+ D:/programmas/ncs/v2.5.0/zephyr/boards/arm/nrf52dk_nrf52832/nrf52dk_nrf52832.dts \
+ D:/programmas/ncs/v2.5.0/zephyr/dts/arm/nordic/nrf52832_qfaa.dtsi \
+ D:/programmas/ncs/v2.5.0/zephyr/dts/common/mem.h \
+ D:/programmas/ncs/v2.5.0/zephyr/dts/arm/nordic/nrf52832.dtsi \
+ D:/programmas/ncs/v2.5.0/zephyr/dts/arm/armv7-m.dtsi \
+ D:/programmas/ncs/v2.5.0/zephyr/dts/common/skeleton.dtsi \
+ D:/programmas/ncs/v2.5.0/zephyr/dts/arm/nordic/nrf_common.dtsi \
+ D:/programmas/ncs/v2.5.0/zephyr/include/zephyr/dt-bindings/adc/adc.h \
+ D:/programmas/ncs/v2.5.0/zephyr/include/zephyr/dt-bindings/adc/nrf-adc.h \
+ D:/programmas/ncs/v2.5.0/zephyr/include/zephyr/dt-bindings/dt-util.h \
+ D:/programmas/ncs/v2.5.0/zephyr/include/zephyr/sys/util_macro.h \
+ D:/programmas/ncs/v2.5.0/zephyr/include/zephyr/sys/util_internal.h \
+ D:/programmas/ncs/v2.5.0/zephyr/include/zephyr/sys/util_loops.h \
+ D:/programmas/ncs/v2.5.0/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ D:/programmas/ncs/v2.5.0/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ D:/programmas/ncs/v2.5.0/zephyr/include/zephyr/dt-bindings/pinctrl/nrf-pinctrl.h \
+ D:/programmas/ncs/v2.5.0/zephyr/include/zephyr/dt-bindings/pwm/pwm.h \
+ D:/programmas/ncs/v2.5.0/zephyr/dts/common/freq.h \
+ D:/programmas/ncs/v2.5.0/zephyr/dts/arm/nordic/override.dtsi \
+ D:/programmas/ncs/v2.5.0/zephyr/boards/arm/nrf52dk_nrf52832/nrf52dk_nrf52832-pinctrl.dtsi \
+ D:/programmas/ncs/v2.5.0/zephyr/include/zephyr/dt-bindings/input/input-event-codes.h
